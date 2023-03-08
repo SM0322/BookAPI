58ec40b5312074ac438eb139557a62efc7d67198
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-08 06:31:14 +0000

    検索結果から1つ選んで保存することができた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,50 +2,49 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-  def search
-    #ここで空の配列を作ります
+  
+  def new
     @books = []
+
     @title = params[:title]
     if @title.present?
-      #この部分でresultsに楽天APIから取得したデータ（jsonデータ）を格納します。
-      #今回は書籍のタイトルを検索して、一致するデータを格納するように設定しています。
       results = RakutenWebService::Books::Book.search({
         title: @title,
+        booksGenreId: '001004',
+        hits: 20,
       })
-      #この部分で「@books」にAPIからの取得したJSONデータを格納していきます。
-      #read(result)については、privateメソッドとして、設定しております。
+
       results.each do |result|
         book = Book.new(read(result))
         @books << book
       end
     end
-    #「@books」内の各データをそれぞれ保存していきます。
-    #すでに保存済の本は除外するためにunlessの構文を記載しています。
-    @books.each do |book|
-      unless Book.all.include?(book)
-        book.save
-      end
-    end
   end
+  
+  def create
+    @book = Book.find_or_initialize_by(isbn: params[:isbn])
 
+    unless @book.persisted?
+      results = RakutenWebService::Books::Book.search(isbn: @book.isbn)
+      @book = Book.new(read(results.first))
+      @book.save
+    end
+ end
+  
   private
-  #「楽天APIのデータから必要なデータを絞り込む」、且つ「対応するカラムにデータを格納する」メソッドを設定していきます。
+
   def read(result)
-    title = result["title"]
-    author = result["author"]
-    url = result["itemUrl"]
-    isbn = result["isbn"]
-    image_url = result["mediumImageUrl"].gsub('?_ex=120x120', '')
-    # book_genre_id = result["booksGenreId"]
-    # item_caption = result["itemCaption"]
+    title = result['title']
+    url = result['itemUrl']
+    isbn = result['isbn']
+    image_url = result['mediumImageUrl'].gsub('?_ex=120x120', '')
+
     {
       title: title,
-      author: author,
       url: url,
       isbn: isbn,
       image_url: image_url,
-    #   book_genre_id: book_genre_id,
-    #   item_caption: item_caption
     }
+
   end
 end
